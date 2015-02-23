a08e77b6fdd404902315b18c5b72d1919c315620
Joas Schilling <nickvergessen@gmx.de>
2015-02-23 12:33:15 +0100

    Etc timezones don't exist for .5 and .75 offsets

--- lib/private/datetimezone.php
+++ lib/private/datetimezone.php
@@ -43,20 +43,43 @@ class DateTimeZone implements IDateTimeZone {
 	public function getTimeZone() {
 		$timeZone = $this->config->getUserValue($this->session->get('user_id'), 'core', 'timezone', null);
 		if ($timeZone === null) {
+			$timeZone = 'UTC';
 			if ($this->session->exists('timezone')) {
 				$offsetHours = $this->session->get('timezone');
-				// Note: the timeZone name is the inverse to the offset,
-				// so a positive offset means negative timeZone
-				// and the other way around.
-				if ($offsetHours > 0) {
-					return new \DateTimeZone('Etc/GMT-' . $offsetHours);
-				} else {
-					return new \DateTimeZone('Etc/GMT+' . abs($offsetHours));
-				}
-			} else {
-				return new \DateTimeZone('UTC');
+				$timeZone = $this->getTimeZoneFromOffset($offsetHours);
 			}
 		}
-		return new \DateTimeZone($timeZone);
+
+		try {
+			return new \DateTimeZone($timeZone);
+		} catch (\Exception $e) {
+			\OCP\Util::writeLog('datetimezone', 'Failed to created DateTimeZone "' . $timeZone . "'", \OCP\Util::DEBUG);
+			return new \DateTimeZone('UTC');
+		}
+	}
+
+	protected function getTimeZoneFromOffset($offset) {
+		if (is_int($offset)) {
+			// Note: the timeZone name is the inverse to the offset,
+			// so a positive offset means negative timeZone
+			// and the other way around.
+			if ($offset == 0 || $offset > 14 || $offset < -12) {
+				return 'UTC';
+			} else if ($offset > 0) {
+				return 'Etc/GMT-' . $offset;
+			} else  {
+				return 'Etc/GMT+' . abs($offset);
+			}
+		}
+
+		foreach (\DateTimeZone::listIdentifiers() as $timeZone) {
+			$dtz = new \DateTimeZone($timeZone);
+			$dtOffset = $dtz->getOffset(new \DateTime());
+			if ($dtOffset == 3600 * $offset) {
+				return $timeZone;
+			}
+		}
+
+		return 'UTC';
 	}
 }
